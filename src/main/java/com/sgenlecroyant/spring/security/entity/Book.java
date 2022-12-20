e0422637df660927cd05b82ac6c87a52fb19e481
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-20 16:40:26 +0200

    added the Book entity model

---
+++ src/main/java/com/sgenlecroyant/spring/security/entity/Book.java
@@ -0,0 +1,56 @@
+package com.sgenlecroyant.spring.security.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity(name = "books")
+@Table(name = "books")
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String title;
+	private String author;
+	private String editionDate;
+
+	public Book() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getEditionDate() {
+		return editionDate;
+	}
+
+	public void setEditionDate(String editionDate) {
+		this.editionDate = editionDate;
+	}
+
+}
