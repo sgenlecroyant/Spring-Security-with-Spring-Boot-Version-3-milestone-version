71d397d086318bc740143c113bc8391e30d89ffc
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-20 16:49:27 +0200

    created the BookRequest model

---
+++ src/main/java/com/sgenlecroyant/spring/security/api/request/BookRequest.java
@@ -0,0 +1,35 @@
+package com.sgenlecroyant.spring.security.api.request;
+
+import java.util.Date;
+
+public class BookRequest {
+
+	private String title;
+	private String author;
+	private Date editionDate;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Date getEditionDate() {
+		return editionDate;
+	}
+
+	public void setEditionDate(Date editionDate) {
+		this.editionDate = editionDate;
+	}
+
+}
