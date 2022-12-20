326dbedf65df0646011aa95fcd9a4b053bd74b78
sgenlecroyant <sgenlecroyant@gmail.com>
2022-12-20 16:25:42 +0200

    initial project set up

---
+++ src/main/java/com/sgenlecroyant/spring/security/SpringSecurityReviseApplication.java
@@ -0,0 +1,13 @@
+package com.sgenlecroyant.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityReviseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityReviseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgenlecroyant/spring/security/SpringSecurityReviseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgenlecroyant.spring.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityReviseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
